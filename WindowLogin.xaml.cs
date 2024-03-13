758aed988965d41d915cf4b0c9158db1bb26c89f
hvmegy <hamegy08lleo@gmail.com>
2024-03-13 16:22:32 +0700

    placeholder for login window

--- WindowLogin.xaml.cs
+++ WindowLogin.xaml.cs
@@ -23,5 +23,43 @@ namespace Window_Final_Term_Projcet__WPF_
         {
             InitializeComponent();
         }
+
+        private void txtUsername_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "Username or email")
+            {
+                txtUsername.Foreground = new SolidColorBrush(Colors.Black); 
+                txtUsername.Text = "";
+            }
+        }
+
+        private void txtUsername_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUsername.Text))
+            {
+                txtUsername.Foreground = new SolidColorBrush(Colors.Gray); 
+                txtUsername.Text = "Username or email"; 
+            }
+        }
+
+        private void txtPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                pabPassword.Password = "";
+                txtPassword.Text = ""; 
+                txtPassword.Visibility = Visibility.Collapsed;
+                pabPassword.Focus(); 
+            }
+        }
+        private void pabPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (pabPassword.Password == "")
+            {
+                txtPassword.Foreground = new SolidColorBrush(Colors.Gray);
+                txtPassword.Visibility = Visibility.Visible; 
+                txtPassword.Text = "Password";
+            }
+        }
     }
 }
