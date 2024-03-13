853eacb9df717db4bac74c027bbcf1da5e463869
Wall_Liu <tuongthanhien@gmail.com>
2024-03-13 12:19:31 +0700

    fix PageCustomer

---
+++ PCustomer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Window_Final_Term_Projcet__WPF_
+{
+    /// <summary>
+    /// Interaction logic for PCustomer.xaml
+    /// </summary>
+    public partial class PCustomer : Page
+    {
+        public PCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
