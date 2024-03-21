5623c3f3378958cc7ab1c67128ae01b04e66af4c
hvmegy <hamegy08lleo@gmail.com>
2024-03-21 19:22:12 +0700

    update code create table

--- DataBase/CreateDataBase.sql
+++ DataBase/CreateDataBase.sql
@@ -1,2 +1,9 @@
-﻿SELECT *
-FROM Room
+﻿CREATE TABLE [dbo].[Room] (
+    [roomID]   INT           IDENTITY (1, 1) NOT NULL,
+    [city]     VARCHAR (100) NULL,
+    [roomType] VARCHAR (100) NULL,
+    [hotel]    VARCHAR (100) NULL,
+    [price]    INT           NULL,
+    [rating]   FLOAT (53)    NULL,
+    PRIMARY KEY CLUSTERED ([roomID] ASC)
+);
