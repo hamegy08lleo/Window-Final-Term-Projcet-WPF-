9930e573eb1a73463f126543cfcdd44169249930
Wall_Liu <tuongthanhien@gmail.com>
2024-04-10 19:21:22 +0700

    test some thing in UCPostChild

--- PAddHotel.xaml.cs
+++ PAddHotel.xaml.cs
@@ -25,16 +25,20 @@ namespace Window_Final_Term_Projcet__WPF_
                         InitializeComponent();
                 }
 
-        private void btnPost_Click(object sender, RoutedEventArgs e)
-        {
-            string hotelName = txtHotelName.Text;
-            string city = cbbCity.Text;
-            string address = txtAddress.Text;  
-            string email = txtEmail.Text;
-            string phoneNumber = txtPhoneNumber.Text;
-            OwnerAddHotel add = new OwnerAddHotel(hotelName, address, city, email, phoneNumber); 
-            HotelDAO hotelDAO = new HotelDAO();
-            hotelDAO.addHotel(add); 
-        }
+        //private void btnPost_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string hotelName = txtHotelName.Text;
+        //    string city = cbbCity.Text;
+        //    string address = txtAddress.Text;  
+        //    string email = txtEmail.Text;
+        //    string phoneNumber = txtPhoneNumber.Text;
+        //    OwnerAddHotel add = new OwnerAddHotel(hotelName, address, city, email, phoneNumber); 
+        //    HotelDAO hotelDAO = new HotelDAO();
+        //    hotelDAO.addHotel(add); 
+        //}
+        private void IsChangeInfor()
+                {
+
+                }
     }
 }
